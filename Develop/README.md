38d3be344c90d7ae731413d5e1c812cff9316177
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-06-07 16:08:45 -0400

    this adds description plus other fields to complete issue 2

--- Develop/README.md
+++ Develop/README.md
@@ -1,13 +1,15 @@
 
-    # Javascript
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#installation)
-    ## Description
-    undefined
-    ## Installation
-    undefined
-    ## Usage
-    undefined
-    ## License
-    ## Questions
+   # Javascript
+   ## Table of Contents
+   - [Description](#description)
+   - [Installation](#installation)
+   ## Description
+   To create a readme
+   ## Installation
+   github
+   ## Usage
+   users who want to create a readme
+   ## Test Instructions
+   test it 
+   ## License
+   ## Questions
--- Develop/index.js
+++ Develop/index.js
@@ -1,61 +1,85 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-
 // // TODO: Create an array of questions for user input
 // const questions = [];
-inquirer 
+inquirer
 .prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the title of your project'
+   {
+       type: 'input',
+       name: 'title',
+       message: 'Enter the title of your project'
+   },
+   {
+       type: 'input',
+       name: 'description',
+       message: 'Enter the description of your project'
+   },
+   {
+       type: 'input',
+       name: 'installation',
+       message: 'Provide the installation intructions of your project'
+   },
+   {
+       type: 'input',
+       name: 'usage',
+       message: 'Provide the usage information of your project'
+   },
+   {
+       type: 'input',
+       name: 'testInstructions',
+       message: 'Provide the test instructions of your project'
+   },
+   {
+    type: 'input',
+    name: 'contributionGuidelines',
+    message: 'How can I contribute to your project?',
     },
 ])
 .then ((answers) => {
-    const readMeContent = generateREADME(answers);
-    fs.writeFile('README.md', readMeContent, (err) => {
-        if (err) throw err;
-        console.log('README.md created successfully!');
-    });
+   const readMeContent = generateREADME(answers);
+   fs.writeFile('README.md', readMeContent, (err) => {
+       if (err) throw err;
+       console.log('README.md created successfully!');
+   });
 })
 .catch ((err) => {
-    console.error(err);
-});
+   console.error(err);
+}),
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 //create the function and pass the answers received into it
-function generateREADME(answers) { 
-    const {
-        title,
-        description,
-        installation,
-        usage,
-        license,
-        contributing,
-        tests,
-        gitHubUsername,
-        email,
-    } = answers
-    const readMeContent = `
-    # ${title}
-    ## Table of Contents
-    - [Description](#description)
-    - [Installation](#installation)
-    ## Description
-    ${description}
-    ## Installation
-    ${installation}
-    ## Usage
-    ${usage}
-    ## License
-    ## Questions`;
+function generateREADME(answers) {
+   const {
+       title,
+       description,
+       installation,
+       usage,
+       testInstructions,
+       contributionGuidelines,
+       //license,
+       // gitHubUsername,
+       // email,
+   } = answers
+   const readMeContent = `
+   # ${title}
+   ## Table of Contents
+   - [Description](#description)
+   - [Installation](#installation)
+   ## Description
+   ${description}
+   ## Installation
+   ${installation}
+   ## Usage
+   ${usage}
+   ## Test Instructions
+   ${testInstructions}
+   ## Contribution Guidelines
+   ${contributionGuidelines}
+   ## License
+   ## Questions`;
 
-    return readMeContent
+
+   return readMeContent
 }
-// // TODO: Create a function to initialize app
-// function init() {}
 
-// // Function call to initialize app
-// init();
