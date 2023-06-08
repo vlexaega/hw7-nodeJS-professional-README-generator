5e262d8f9246f7fd08e3b97bcb53b3625595284d
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-06-07 21:00:48 -0400

    further saves to licenses

--- Develop/README.md
+++ Develop/README.md
@@ -1,18 +1,20 @@
 
-   # node index undefined
+   # Javascript Challenge https://img.shields.io/badge/License-MIT-yellow.svg
    ## Table of Contents
    - [Description](#description)
    - [Installation](#installation)
    ## Description
-   
+   The purpose of this project is to utilize Node.js in order to link multiple javascript files and receive input from the user and prepare a professional README.md with their input. 
    ## Installation
-   j
+   This repo can be cloned at github.com/vlexaega. Once downloaded, run your terminal and initialize npm then you will have access to the files. 
    ## Usage
-   yet
+   The user of this project will be seeking help to prepare a professional README directly within their existing work. 
    ## Test Instructions
-   yes
+   Test can be conducted by utilizing your terminal and once npm initialized, utilizing Node Index. 
    ## Contribution Guidelines
-   yes
-   ## License
-   You chose the MIT License license. For more details, please visit the [license page](undefined).
+   You can email me at alx.vga2@gmail.com. 
+   
+## License
+This application is covered under the [MIT License](P) license. https://img.shields.io/badge/License-MIT-yellow.svg
+
    ## Questions
--- Develop/index.js
+++ Develop/index.js
@@ -1,36 +1,35 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs'); //this creates files
+const fs = require('fs'); // This creates files
 const licenses = require('./utils/licenses.json');
-const { generateMarkdown } = require('./utils/generateMarkdown'); //adding the curly brackets 'destructures' the generateMarkdown.js file
-const licenseLinks = require('./utils/licenses');
-
+const { generateMarkdown, getLicenseNames } = require('./utils/generateMarkdown');
 
 // Function to generate README content
 function generateREADME(answers) {
-   const {
-       title,
-       description,
-       installation,
-       usage,
-       testInstructions,
-       contributionGuidelines,
-       license,
-       // gitHubUsername,
-       // email,
-   } = answers;
-   //get the license badge
-   const licenseBadge = generateMarkdown(answers).licenseBadge;//targets an object in the generateMarkdown file with a licenseBadge property
-   //get the license object based on license name
-   const targetLicense = licenses.find((item) => item.name === license);
-   //if object is found, retrieve the license link
-   const licenseLink = targetLicense ? targetLicense.link : '';
-   //create the readme content
-   const readMeContent = `
-   # ${title} ${licenseBadge}
+  const {
+    title,
+    description,
+    installation,
+    usage,
+    testInstructions,
+    contributionGuidelines,
+    license,
+    questions,
+  } = answers;
+
+  // Create the markdown data using the generateMarkdown function
+  const markdownData = generateMarkdown(answers);
+
+  // Create the README content
+  const readMeContent = `
+   # ${markdownData.title} ${markdownData.badge}
    ## Table of Contents
    - [Description](#description)
    - [Installation](#installation)
+   - [Usage](#usage)
+   - [Test Instructions](#testInstructions)
+   - [Contribution Guidelines](#contributionGuidelines)
+   - [Questions](#questions)
    ## Description
    ${description}
    ## Installation
@@ -41,61 +40,66 @@ function generateREADME(answers) {
    ${testInstructions}
    ## Contribution Guidelines
    ${contributionGuidelines}
-   ## License
-   You chose the ${license} license. For more details, please visit the [license page](${licenseLink}).
-   ## Questions`;
+   ${markdownData.license}
+   ## Questions
 
-   return readMeContent;
+   You can reach me via email at alx.vga2@gmail.com
+   `;
+  return readMeContent;
 }
 
 // Prompt the user for input
 inquirer
-.prompt([
-   {
-       type: 'input',
-       name: 'title',
-       message: 'Enter the title of your project'
-   },
-   {
-       type: 'input',
-       name: 'description',
-       message: 'Enter the description of your project'
-   },
-   {
-       type: 'input',
-       name: 'installation',
-       message: 'Provide the installation instructions of your project'
-   },
-   {
-       type: 'input',
-       name: 'usage',
-       message: 'Provide the usage information of your project'
-   },
-   {
-       type: 'input',
-       name: 'testInstructions',
-       message: 'Provide the test instructions of your project'
-   },
-   {
-        type: 'input',
-        name: 'contributionGuidelines',
-        message: 'How can I contribute to your project?',
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of your project',
     },
     {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license for your project:',
-        choices: licenseLinks.map((license) => license.name),
+      type: 'input',
+      name: 'description',
+      message: 'Enter the description of your project',
     },
-])
-.then ((answers) => {
-   const readMeContent = generateREADME(answers);
-   fs.writeFile('README.md', readMeContent, (err) => {
-       if (err) throw err;
-       console.log('README.md created successfully!');
-   });
-})
-.catch ((err) => {
-   console.error(err);
-});
-
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide the installation instructions of your project',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide the usage information of your project',
+    },
+    {
+      type: 'input',
+      name: 'testInstructions',
+      message: 'Provide the test instructions of your project',
+    },
+    {
+      type: 'input',
+      name: 'contributionGuidelines',
+      message: 'How can I contribute to your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: getLicenseNames(),
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'How can I reach you?',
+      },
+  ])
+  .then((answers) => {
+    const readMeContent = generateREADME(answers);
+    fs.writeFile('README.md', readMeContent, (err) => {
+      if (err) throw err;
+      console.log('README.md created successfully!');
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
